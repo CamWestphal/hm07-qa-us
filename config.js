f20f8b43abf7060ef109f4a08c373b87e9ed67b5
Cameron Westphal <cam.micaiah@gmail.com>
2023-11-21 20:10:28 -0800

    second submission

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://d580d020-7230-476a-80fb-31979ce3f194.serverhub.tripleten-services.com"
+    API_URL: "https://e9847791-bee4-4c72-b8b2-bd704030a52f.serverhub.tripleten-services.com"
 }
--- tests/putHandlers.test.js
+++ tests/putHandlers.test.js
@@ -1,7 +1,8 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
 
-const requestBody = {"productsList":[
+const putRequestBody = {
+	"productsList":[
 		{
 			"id": 1,
 			"quantity": 4
@@ -10,15 +11,35 @@ const requestBody = {"productsList":[
 	"name": "new cart"
 }
 
+//this is to create the kit necessary for the test
+const postRequestBody = {
+		"name": "My set",
+		"card": {
+			"id": 1,
+			"name": "For the situation"
+		},
+		"productsList": null,
+		"id": 7,
+		"productsCount": 0
+}
+
 test('status should be 200', async () => {
 	let actualStatusCode;
     try {
+		//creating the kit before the test to make sure that the endpoint for the
+		const createKit = await fetch(`${config.API_URL}/api/v1/kits`, {
+			method: 'POST',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(postRequestBody)
+		});
 		const response = await fetch(`${config.API_URL}/api/v1/kits/7`, {
 			method: 'PUT',
 			headers: {
 			'Content-Type': 'application/json'
 			},
-			body: JSON.stringify(requestBody)
+			body: JSON.stringify(putRequestBody)
 		});
 		actualStatusCode = response.status;
 	} catch (error) {
@@ -35,7 +56,7 @@ test('Request body should be ok true', async () => {
 			headers: {
 			'Content-Type': 'application/json'
 			},
-			body: JSON.stringify(requestBody)
+			body: JSON.stringify(putRequestBody)
 		});
 		actualResponseBody = await response.json();
 	} catch (error) {
